b5f02015a40f360a3481d93399b08ec2e804deb3
Aiky30 <Aiky30@users.noreply.github.com>
2021-10-04 10:01:50 +0100

    fix: [gaps-215] - Add the ability to use polymorphic content types if found (#25)

--- djangocms_content_expiry/utils.py
+++ djangocms_content_expiry/utils.py
@@ -4,13 +4,27 @@ from .conf import DEFAULT_CONTENT_EXPIRY_DURATION
 from .models import DefaultContentExpiryConfiguration
 
 
+# FIXME: Due to time constraints this function is not covered by unit tests
+def _get_version_content_model_content_type(version):
+    """
+    Returns a content type that describes the content, this is especially
+    important for polymorphic models which would otherwise return the wrong content type!
+    """
+    # If the version identifies as a different content type, be sure to use it
+    if hasattr(version.content, "polymorphic_ctype"):
+        return version.content.polymorphic_ctype
+    # Otherwise, use the content type registered by the version
+    return version.content_type
+
+
 def get_default_duration_for_version(version):
     """
     Returns a default expiration value dependant on whether an entry exists for
     a content type in DefaultContentExpiryConfiguration.
     """
+    content_type = _get_version_content_model_content_type(version)
     default_configuration = DefaultContentExpiryConfiguration.objects.filter(
-        content_type=version.content_type
+        content_type=content_type
     )
     if default_configuration:
         return relativedelta(months=default_configuration[0].duration)
